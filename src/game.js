16b0d241c0c96a923e47e267c9e92ec9cc6a2757
Sundeep Chand <sundeep.chand2001@gmail.com>
2019-11-03 11:24:48 +0530

    Added game menu

--- src/game.js
+++ src/game.js
@@ -1,6 +1,31 @@
 class Game {
     constructor() {
-        this.gameBoard = new Board()   
+        this.initGame()
+    }
+
+    initGame() {
+        this.gameMenu = document.getElementById('game-menu')    // Select the game menu.
+        this.btnStart = document.getElementById('btn-start')    // Select the start button.
+
+        this.btnStart.addEventListener('click', () => {
+            this.startNewGame()
+        })
+    }
+
+    hideGameMenu() {
+        // Close the game board.
+        this.gameMenu.style.opacity = '0'
+        setTimeout(() => {
+            this.gameMenu.style.visibility = 'hidden'
+        }, 500)
+    }
+
+    startNewGame() {
+        // Start game.
+        this.hideGameMenu()
+        
+        // Start the game.
+        this.gameBoard = new Board() 
 
         // Call handleGameOver every 500ms.
         this.gameOverInterval = window.setInterval(() => {
@@ -12,7 +37,7 @@ class Game {
         // Checks if the ame is over.
         if (this.gameBoard.isGameOver()) {
             window.clearInterval(this.gameOverInterval)
-            alert('Game is over now')
+            // alert('Game is over now')
         }
     }
 }
