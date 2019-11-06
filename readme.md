fde97adb93914f8d8e6532528514188b79c9b765
Sundeep Chand <sundeep.chand2001@gmail.com>
2019-11-07 00:06:47 +0530

    Updated Readme.md

--- readme.md
+++ readme.md
@@ -1,1 +1,12 @@
-# Application under construction
+# Memory-Match
+-----------------
+
+> Play it [here!](https://sundeepchand.github.io/Memory-Match/)
+
+A web-based memory game where the player selects cards having same pictures in a row to score the maximum points in minimum time possible.
+
+## Uses HTML, CSS and Vanilla JS
+
+## Screenshot
+
+![Scrrenshot of the game-screen](img/screenshot.png)
--- src/currentTime.js
+++ src/currentTime.js
@@ -21,14 +21,14 @@ class currentTime {
         // renders the time on the stopWatchElement
         let minutesText = this.minutes.toString()
         let secondsText = this.seconds.toString()
-        // if (this.seconds < 10)
-        // {
-        //     secondsText = '0' + this.seconds.toString()
-        // }
-        // if (this.minutes < 10)
-        // {
-        //     minutesText = '0' + this.minutes.toString()
-        // }
+        if (this.seconds < 10)
+        {
+            secondsText = `0${this.seconds.toString()}`
+        }
+        if (this.minutes < 10)
+        {
+            minutesText = `0${this.minutes.toString()}`
+        }
         this.stopwatchElement.textContent = `${minutesText}:${secondsText}`
     }
 }
